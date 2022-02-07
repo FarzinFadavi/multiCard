f9eab062496964eff48e51054cc8fc7ea8066436
ffa <falsh.tester@gmail.com>
2022-02-07 14:26:25 +0330

    Initial Commit

---
+++ multiCard/multiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiCardApp.swift
+//  multiCard
+//
+//  Created by Farzin macbook on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct multiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
